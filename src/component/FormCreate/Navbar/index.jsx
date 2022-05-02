41f36df9c854f13eca74159428ce40c20b99a1ed
unknown <hardikpatel422>
2022-05-02 14:19:07 +0530

    first commit

---
+++ src/component/FormCreate/Navbar/index.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary ">
+            <a className="navbar-brand m-10 " href="/FormCreate">AddData</a>            
+            <a className="navbar-brand" href="/FormList">ShowData</a>
+            
+        </nav>
+      
+  )
+}
+
+export default Navbar
---
+++ src/component/FormList/index.jsx
@@ -0,0 +1,88 @@
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+import { AddUser,DeleteUser } from "../../actions/UserAction";
+
+
+
+export default function FormList() {
+  let history = useHistory();
+  const dispatch = useDispatch();
+
+  const submithandler = () => {
+    
+      
+    history.push("/ContactForm");
+  }
+//   const edithandler = (data) => {
+      
+//     dispatch(EditContact(data));
+//     history.push("/ContactForm");
+//   }
+  
+  const userSelector = useSelector((state) => state.users.users)
+
+  return (
+    <div className="container">
+      <div className="row d-flex flex-column">
+
+        <div className="col-md-12 mx-auto my-4 mx-auto shadow">
+          <table className="table table-hover">
+            <thead className="table-header bg-dark text-white">
+              <tr>
+                <th scope="col">Id</th>
+                <th scope="col">First Name</th>
+                <th scope="col">Last Name</th>
+                <th scope="col">Email</th>
+                <th scope="col">Gender</th>
+                <th scope="col">Martial Status</th>
+                <th scope="col">Members</th>
+                <th scope="col"></th>
+              </tr>
+            </thead>
+            <tbody>
+              {userSelector.map(users => (
+                <tr>
+                  <td>{users.id}</td>
+                  <td>{users.FirstName}</td>
+                  <td>{users.LastName}</td>
+                  <td>{users.Email}</td>
+                  <td>{users.Gender}</td>
+                  <td>{users.MartialStatus}</td>
+                  {/* <td>{users.Member.map(function(name, index){
+                    return <li key={ index }>{name}</li>;
+                  })}</td> */}
+
+                  <td>
+                    {users.Member.map((item) => (
+                      <li key={item.mid}>{item.name}</li>
+                    ))}
+                  </td>
+                  <td>
+
+                    <button
+                      type="button"
+                      onClick={() => dispatch(DeleteUser(users.id))}
+                      // onClick={deleteda}
+                      className="btn btn-sm btn-danger mx-3"
+                    >
+                      Delete
+                    </button>
+                   
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          {/* <button type="button" onClick={submithandler} className="btn btn-outline-dark btn-sm ">Add Contact</button> */}
+        </div>
+      </div>
+    </div>
+
+  );
+
+
+
+}
